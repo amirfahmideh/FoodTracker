2ca6ebc32efaf212d558b6dd9ba069675a9d8c83
Amir Fahmideh VatanDost <amir.fahmideh@gmail.com>
2018-02-18 02:54:23 -0800

    add text filed delegate

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,21 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet var lblMealTitle: UILabel!
     @IBOutlet var txtMealTitle: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        txtMealTitle.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
+    
     }
+    //Mark UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtMealTitle.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        lblMealTitle.text = txtMealTitle.text
+    }
+    
     @IBAction func setMealNameTitle(_ sender: UIButton) {
         lblMealTitle.text = "Default Label"
     }
-    
+
 }
 
